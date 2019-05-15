2e505c827cd45bcbc2232ffce9624c48f92a2c34
Victoria Ledsom <victorialedsom@gmail.com>
2019-05-15 16:22:27 +0100

    Make icon clickable and hide

---
+++ main.js
@@ -0,0 +1,61 @@
+const { app, Tray, BrowserWindow } = require('electron');
+
+// Hide the app from the dock
+app.dock.hide();
+
+let window;
+let tray;
+const createWindow = () => {
+  window = new BrowserWindow({
+    width: 210,
+    height: 210,
+    frame: false,
+    transparent: false,
+    fullscreenable: false,
+    resizable: false,
+    'node-integration': false,
+  });
+  window.loadURL(`file://${__dirname}/index.html`)
+};
+
+const getWindowPosition = () => {
+  const windowBounds = window.getBounds()
+  const trayBounds = tray.getBounds()
+
+  // Center window horizontally below the tray icon
+  const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2))
+
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y + trayBounds.height + 4)
+
+  return {x: x, y: y}
+}
+
+const showWindow = () => {
+  const position = getWindowPosition();
+  window.setPosition(position.x, position.y, false);
+
+  window.show();
+  window.focus();
+  window.open = true;
+
+  window.on('blur', () => {
+    window.hide();
+  });
+};
+
+// Tray is the icon that sits in the top toolbar of Mac windows.
+const createTray = () => {
+  tray = new Tray('./images/tomato16.png');
+  tray.on('click', () => {
+    if(window && window.open) {
+      window.hide();
+    }
+    createWindow();
+    showWindow();
+  });
+};
+
+app.on('ready', () => {
+  createTray();
+});
