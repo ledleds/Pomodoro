77434fc4e24bf63ca1fab29e9fe4eed955b36b6f
Victoria Ledsom <victorialedsom@gmail.com>
2019-05-16 16:12:36 +0100

    Add countdown timer

--- timer.js
+++ timer.js
@@ -1,24 +1,34 @@
 const electron = window.require('electron');
+const dayjs = window.require('dayjs');
 
 const notification = ({ title, body }) => {
   new electron.remote.Notification({ title, body }).show();
 };
 
 const startCountdown = (minutes = 25) => {
-  let counter = minutes;
+  const end = dayjs().add(minutes, 'minutes');
 
   const interval = setInterval(() => {
-    console.log(`${counter} mins left`);
-    counter -= 1;
+    const now = new Date().getTime();
+    let time = new Date(end).getTime() - now;
 
-    if (counter < 0) {
+    if (time >= 0) {
+
+    const mins =  Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds =  Math.floor((time % (1000 * 60)) / 1000);
+
+    document.getElementById("mins").innerHTML = ("0" + mins).slice(-2)
+
+    document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2)
+
+    } else {
       clearInterval(interval);
       notification({
         title: 'Pomodoro',
         body: 'Great work! Time to take a 5 minute break.',
       });
     }
-  }, 60000);
+  }, 1000);
 };
 
 document.getElementById('start').addEventListener('click', () => {
